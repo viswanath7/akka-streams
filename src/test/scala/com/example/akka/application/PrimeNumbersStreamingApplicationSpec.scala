7bc4688f79918ed7cdcd4af131f0126fa869c486
Jayachandran, Viswanath <Viswanath.Jayachandran@aegon.nl>
2017-08-12 22:33:37 +0200

    Added tests

--- src/test/scala/com/example/akka/application/PrimeNumbersStreamingApplicationSpec.scala
+++ src/test/scala/com/example/akka/application/PrimeNumbersStreamingApplicationSpec.scala
@@ -2,12 +2,14 @@ package com.example.akka.application
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, MustMatchers}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.{Failure, Success}
 
 
 class PrimeNumbersStreamingApplicationSpec extends TestKit(ActorSystem("test-actor-system"))
@@ -27,6 +29,25 @@ class PrimeNumbersStreamingApplicationSpec extends TestKit(ActorSystem("test-act
 
     computationResult must have size 26
     computationResult must contain allElementsOf List(1, 2, 3, 5, 7, 11)
+  }
+
+
+  "Source" should "contain correct elements" in {
+    val source = StreamDefinition.source
+    import system.dispatcher
+
+    source.runWith(Sink.fold(0)((a, b) => a + 1)).onComplete {
+      case Success(numOfElements) => numOfElements mustBe 100
+      case Failure(err) => fail(err)
+    }
+  }
+
+  "Flow" should "do right the right filtering transformation" in {
+    val flow = StreamDefinition.flow
+
+    val result = Source(1 to 10).via(flow).runWith(Sink.fold(Seq.empty[Int])(_ :+ _))
+
+    Await.result(result, 100.millis) must contain theSameElementsInOrderAs List(1, 2, 3, 5, 7)
   }
 
 
