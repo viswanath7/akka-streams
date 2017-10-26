6f0e83b8b57038069129455399cd355de8287c67
Jayachandran, Viswanath <Viswanath.Jayachandran@aegon.nl>
2017-10-26 11:48:56 +0200

    Minor enhancements

--- src/test/scala/com/example/akka/application/PrimeNumbersStreamingApplicationSpec.scala
+++ src/test/scala/com/example/akka/application/PrimeNumbersStreamingApplicationSpec.scala
@@ -7,8 +7,8 @@ import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, MustMatchers}
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
 import scala.util.{Failure, Success}
 
@@ -27,6 +27,8 @@ class PrimeNumbersStreamingApplicationSpec extends TestKit(ActorSystem("test-act
     val flow = StreamDefinition.flow
     val sink = StreamDefinition.sink
 
+    logger debug "Testing sink containing a collection of prime numbers ..."
+    
     val resultFuture = source.via(flow).runWith(sink)
     val computationResult = Await.result(resultFuture, 100 millis)
 
@@ -38,8 +40,16 @@ class PrimeNumbersStreamingApplicationSpec extends TestKit(ActorSystem("test-act
   "Source" should "contain correct elements" in {
     val source = StreamDefinition.source
     import system.dispatcher
-
-    source.runWith(Sink.fold(0)((a, b) => a + 1)).onComplete {
+  
+    logger debug "Testing source generating numbers from 1 to 100 ..."
+  
+    /**
+      * Sink that invokes the supplied fold function for every element it receives
+      * Here the fold function counts the number of elements in the source stream
+      */
+    val counterSink: Sink[Int, Future[Int]] = Sink.fold(0)((a, b) => a + 1)
+    
+    source.runWith(counterSink).onComplete {
       case Success(numOfElements) => numOfElements mustBe 100
       case Failure(err) => fail(err)
     }
@@ -47,7 +57,19 @@ class PrimeNumbersStreamingApplicationSpec extends TestKit(ActorSystem("test-act
 
   "Flow" should "do right the right filtering transformation" in {
     val flow = StreamDefinition.flow
-    val result = Source(1 to 10).via(flow).runWith(Sink.fold(Seq.empty[Int])(_ :+ _))
+    
+    logger debug "Testing flow that filters for prime numbers ..."
+  
+    /**
+      * Sink that invokes the supplied fold function for every element it receives
+      * Here the fold function collects the elements into a sequence
+      */
+    val elementsCollectorSink: Sink[Int, Future[Seq[Int]]] = Sink.fold(Seq.empty[Int])(_ :+ _)
+    
+    val result = Source(1 to 10)
+      .via(flow)
+      .runWith(elementsCollectorSink)
+    
     Await.result(result, 100.millis) must contain theSameElementsInOrderAs List(1, 2, 3, 5, 7)
   }
 
