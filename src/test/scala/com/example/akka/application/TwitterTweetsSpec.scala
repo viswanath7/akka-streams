a9d9d4f7cb9d29227f878dc765b70eca924851a9
Jayachandran, Viswanath <Viswanath.Jayachandran@aegon.nl>
2017-10-27 12:14:40 +0200

    Increased test coverage

--- src/test/scala/com/example/akka/application/TwitterTweetsSpec.scala
+++ src/test/scala/com/example/akka/application/TwitterTweetsSpec.scala
@@ -1,15 +1,17 @@
 package com.example.akka.application
 
-import akka.actor.{ActorSystem, Cancellable}
+import akka.NotUsed
+import akka.actor.{ActorRef, ActorSystem, Cancellable}
 import akka.pattern.pipe
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import com.example.akka.{Author, Tweet}
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FlatSpecLike, MustMatchers}
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
 import scala.reflect._
 import scala.util.Random
@@ -36,18 +38,26 @@ class TwitterTweetsSpec extends TestKit(ActorSystem("test-actor-system"))
 	}
 	
 	override def afterAll {
+		logger debug "Shutting down actor system ..."
 		TestKit.shutdownActorSystem(system)
 	}
 	
-	"With TestKit" should "test actor receive elements from the sink" in {
+	override def beforeAll {
+		logger debug "Testing with akka test-kit ..."
+	}
+	
+	"Source" should "supply a stream of tweets" in {
 		logger debug "Testing source to supply a stream of tweets ..."
 		val testProbe: TestProbe = TestProbe()
-		val source = Source.fromIterator(() => sampleTweets.iterator)
-
-		source.runWith(Sink.head)
+		val atomicSource = Source.fromIterator(() => sampleTweets.iterator)
+		
+		logger debug "Connecting source with sink and piping the elements from sink to test actor ..."
+		atomicSource
+			.runWith(Sink.head) // Only the first value received by the sink shall be materialised
 			.pipeTo(testProbe.ref)
 		
 		testProbe.expectMsg(sampleTweets.head)
+		testProbe.expectNoMsg()
 	}
 	
 	"Sink" should "receive messages from source" in {
@@ -69,12 +79,44 @@ class TwitterTweetsSpec extends TestKit(ActorSystem("test-actor-system"))
 		logger debug "Connecting source to sink and running it ..."
 		val materialisedInstance: Cancellable = source.to(sink).run
 		
-		testProbe.expectMsgAnyOf(1 second, sampleTweets:_*)
+		testProbe.expectMsgAnyOf(1 second, sampleTweets: _*)
 		testProbe expectNoMsg (300 milliseconds)
-		testProbe.expectMsgAnyOf(250 milliseconds, sampleTweets:_*)
+		testProbe.expectMsgAnyOf(250 milliseconds, sampleTweets: _*)
 		materialisedInstance.cancel()
 		testProbe.expectMsg[String](300 milliseconds, "completed")
 		testProbe.expectNoMsg()
+	}
+	
+	"With source actor, one " should " have control over tweets sent to processing stream" in {
+		/*
+		* Creating a source materialised as an actor where messages sent to this actor will be emitted to the stream,
+		* if there is demand from downstream, otherwise they will be buffered until request for demand is received.
+		*/
+		logger debug "Defining source materialised as an actor with fixed buffer size of 10."
+		logger debug "New elements arriving to source shall be dropped when its buffer is full"
+		val source = Source actorRef(bufferSize = 10, overflowStrategy = OverflowStrategy.dropNew)
+		
+		logger debug "Defining a flow that extracts the body of the tweet ..."
+		val flow: Flow[Tweet, String, NotUsed] = Flow[Tweet].map(_.body)
+		
+		logger debug "Connecting flow to sink that concatenates the elements it receives ..."
+		/**
+			* Note on usage of combining flow with sink.
+			*
+			* Use the method 'toMat' to materialise, so that one can specify what to retain as result (via the combine function) when connecting flow to a sink.
+			* Materialised value of provided sink shall be materialised value of combined / concatenated process flow
+			* The method 'to' retains materialised value of flow as the combined materialised value; which is undesirable in our case.
+			*/
+		val sink: Sink[Tweet, Future[String]] = flow.toMat(Sink.fold("")((accumulatedOutput, element) => accumulatedOutput + element + " | "))(Keep.right)
+		
+		val sourceActorResultPair: (ActorRef, Future[String]) = source.toMat(sink)(Keep.both).run()
+		
+		sourceActorResultPair._1 ! sampleTweets.head
+		sourceActorResultPair._1 ! sampleTweets(1)
+		sourceActorResultPair._1 ! akka.actor.Status.Success("done")
+		
+		Await.result(sourceActorResultPair._2, 500 milliseconds) mustBe
+			"Any #Scala companies in #Porto on the lookout for developers? Asking for a friend. RTs welcome. | @scala_sbt: sbt 1.0.0 is now available, ft #Scala 2.12 and Zinc 1! Thanks to all contributors and plugins! @lightbend | "
 	}
 	
 }
